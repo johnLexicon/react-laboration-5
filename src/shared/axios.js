ec10ded9d837d8beea80799d9c68d631c36bd967
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-07-04 20:02:47 +0200

    Changed to own user backend.

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const config = {
-  baseURL: 'https://jsonplaceholder.typicode.com/users',
+  baseURL: 'https://lexicon-express-users.herokuapp.com/api/users',
+  // baseURL: 'https://jsonplaceholder.typicode.com/users',
 };
 
 export default axios.create(config);
