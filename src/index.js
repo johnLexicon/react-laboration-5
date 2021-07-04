c30c7c3321385749756f00b1aafd1a87e978a666
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-07-04 10:57:10 +0200

    Created redux store.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import usersReducer from './store/users';
 import App from './App';
 
+const store = createStore(usersReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
