76baa216253977ff80c3a80d81b8238cc697d841
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-07-04 11:50:34 +0200

    Added redux devtools.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import usersReducer from './store/users';
 import App from './App';
 
-const store = createStore(usersReducer, applyMiddleware(thunk));
+const store = createStore(
+  usersReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
