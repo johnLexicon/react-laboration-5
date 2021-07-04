87edf30e631ffaefde2fa5606e5b051e64071fee
John Lundgren <john.lundgren.lexicon@gmail.com>
2021-07-04 20:16:34 +0200

    Added navigation on edit user.

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -1,4 +1,9 @@
+import { useHistory } from 'react-router-dom';
 const UserCard = ({ user }) => {
+  const history = useHistory();
+  const editUser = () => {
+    history.push(`/edit/${user._id}`);
+  };
   return (
     <div className="card me-3 shadow-sm my-5" style={{ width: '18rem' }}>
       <img
@@ -11,7 +16,9 @@ const UserCard = ({ user }) => {
         <h5 className="card-title">{user.name}</h5>
         <p className="card-text">{user.email}</p>
         <div className="d-flex justify-content-around">
-          <button className="btn btn-primary">Edit</button>
+          <button onClick={editUser} className="btn btn-primary">
+            Edit
+          </button>
           <button className="btn btn-danger">Remove</button>
         </div>
       </div>
